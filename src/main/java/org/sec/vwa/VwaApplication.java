83e6f50ef8c5f37050746c4b46660f2f92ca1da0
Kleber Almeida Toyota <kleber@localhost.localdomain>
2016-12-14 17:20:38 -0200

    Initial commit

---
+++ src/main/java/org/sec/vwa/VwaApplication.java
@@ -0,0 +1,12 @@
+package org.sec.vwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VwaApplication.class, args);
+	}
+}
