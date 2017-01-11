80159a356d66eda10cd194764b36308f4948a432
Kleber Almeida Toyota <klebertiko@gmail.com>
2017-01-11 20:40:53 -0200

    Adicionando problemas de segurança

--- src/main/java/org/sec/vwa/VWAMvcConfiguration.java
+++ src/main/java/org/sec/vwa/VWAMvcConfiguration.java
@@ -9,8 +9,7 @@ public class VWAMvcConfiguration extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("login");
-		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/home").setViewName("home");
 		registry.addViewController("/banana").setViewName("banana");
 	}
